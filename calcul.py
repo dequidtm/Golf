aa7676138a12ceaf7e8ccfedef282096990dea5f
dequidt matthis <dequidtm@gmail.com>
2019-09-18 17:09:20 +0200

    modif1

--- calcul.py
+++ calcul.py
@@ -1,17 +1,28 @@
 
 import confBDD
 import math
- 
-connection = confBDD.getConnection()
- 
-print ("Connect successful!")
 
-def calcul_arrosage(ID_capteur, temperature, humidité_air, humidité_sol):
+
+def calculDPV(temperature, humidité_air):
     SVP = math.exp((13.7 - 5120 / ( temperature + 273.15 ))) * 1000
     print (SVP)
     DPV =  (( 100 - humidité_air ) / 100) * SVP
     print (DPV)
+    return (DPV)
+
+def calcul_arrosage(ID_capteur, temperature, humidité_air, humidité_sol):
+    DPV = calculDPV(temperature, humidité_air)
     connection = confBDD.getConnection()
+    if (DPV < 4.5 && DPV > 12.5 && humidité_sol < 15 && humidité_sol > 30):
+        
+    elif
+
+    try:
+    with connection.cursor() as cursor:
+        sql = ""
+        cursor.execute(sql)
+    finally:     
+    connection.close()
+    return 0
 
 
-    return 0;
--- confBDD.py
+++ confBDD.py
@@ -2,12 +2,12 @@ import pymysql.cursors
  
 # La fonction renvoie une connexion.
 def getConnection():
-     
-    # Vous pouvez changer les arguments de la connexion.
+
     connection = pymysql.connect(host='localhost',
                                  user='root',
                                  password='',                             
                                  db='mysql',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
+    print ("connected")
     return connection
