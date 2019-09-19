16e1210b5740a64a4bb5a2aabf5a9c8563a32a19
dequidt matthis <dequidtm@gmail.com>
2019-09-19 09:31:59 +0200

    Modif SQL

--- creationBDD.sql
+++ creationBDD.sql
@@ -1,17 +1,40 @@
-﻿CREATE DATABASE GOLF CHARACTER SET "utf8mb4";
-USE GOLF;
-CREATE TABLE CAPTEUR (
-	ID_CAPTEUR VARCHAR(40),
-    LOC_CAPTEUR VARCHAR(40),
-    PRIMARY KEY (ID_CAPTEUR)
+﻿CREATE DATABASE ROC CHARACTER SET "utf8mb4";
+USE ROC;
+CREATE TABLE USER (
+	user_id integer,
+    user_name varchar(40),
+    user_email varchar(40),
+    user_pwd varchar(40),
+    PRIMARY KEY (user_id)
 );
-CREATE TABLE MESURE (
-	DATE_MESURE DATETIME NOT NULL,
-    ID_CAPTEUR VARCHAR(40),
-    TEMPERATURE FLOAT,
-    HUMIDITE_AIR FLOAT,
-    HUMIDITE_SOL FLOAT,
-    ARROSAGE VARCHAR(5),
-    PRIMARY KEY (DATE_MESURE),
-    CONSTRAINT fk_id_capteur FOREIGN KEY (ID_CAPTEUR) REFERENCES CAPTEUR(ID_CAPTEUR)
+CREATE TABLE GOLF (
+    golf_id integer,
+    golf_name integer,
+    golf_installday datetime,
+    golf_watersaved float,
+    PRIMARY KEY (golf_id)
+);
+CREATE TABLE WATER (
+    water_id integer,
+    water_day varchar(40),
+    water_start float,
+    golf_end float,
+    PRIMARY KEY (water_id)
+);
+CREATE TABLE ARDUINO (
+    arduino_id integer,
+    arduino_ref varchar(40),
+    arduino_latitude float,
+    arduino_longitude float,
+    arduino_battery float,
+    PRIMARY KEY (arduino_id)
+);
+CREATE TABLE INFO (
+	info_id integer AUTO_INCREMENT,
+    info_type integer,
+    info_arduino integer,
+    info_value float,
+    info_date datetime,
+    PRIMARY KEY (info_id),
+    CONSTRAINT fk_id_arduino FOREIGN KEY (info_arduino) REFERENCES ARDUINO(arduino_id)
 );
