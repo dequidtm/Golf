21992827f43a93986a2135ccbbd12e27b5b52590
dequidt matthis <dequidtm@gmail.com>
2019-09-26 14:59:10 +0200

    ok

--- CSV_golf.py
+++ CSV_golf.py
@@ -1,1 +1,20 @@
 
+import csv
+
+def csv_exist(nom_fichier):
+    try:
+        f = open(nom_fichier, 'r', newline='')
+        return True
+    except :
+        return False
+        
+
+def inserer(nom_fichier, liste_valeur):
+    with open(nom_fichier, 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(liste_valeur)
+
+
+    
+    
+
--- Golf.py
+++ Golf.py
@@ -1,5 +1,7 @@
 import calcul
+import CSV_golf 
 from threading import Thread
-
+print(CSV_golf.csv_exist('test1.csv'))
+CSV_golf.inserer('test.csv',["1;2;3"])
 Thread.start(calcul.calcul_arrosage(1,15,50,25))
 
