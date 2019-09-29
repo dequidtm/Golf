9ed4a574c249015b3e8acf2a446760760ee5fd8f
dequidt matthis <dequidtm@gmail.com>
2019-09-29 19:01:10 +0200

    CSV OK

--- CSV_golf.py
+++ CSV_golf.py
@@ -7,13 +7,11 @@ def csv_exist(nom_fichier):
         return True
     except :
         return False
-        
 
 def inserer(nom_fichier, liste_valeur):
-    with open(nom_fichier, 'a', newline='') as f:
-        writer = csv.writer(f)
+    with open(nom_fichier, 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=';')
         writer.writerow(liste_valeur)
-
 
     
     
--- calcul.py
+++ calcul.py
@@ -1,6 +1,7 @@
 
 import confBDD
 import math
+import CSV_golf
 from datetime import datetime
 
 
@@ -14,6 +15,8 @@ def calculDPV(temperature, humidité_air):
 
 def calcul_arrosage(arduino_id, temperature, humidité_air, humidité_sol):
     DPV = calculDPV(temperature, humidité_air)
+    String_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    CSV_name = datetime.now().strftime("%Y-%m-%d") + '.csv'
     connection = confBDD.getConnection()
     if (DPV > 4.5 and DPV < 12.5 and humidité_sol > 15 and humidité_sol < 30):
         arrosage = 0
@@ -21,13 +24,18 @@ def calcul_arrosage(arduino_id, temperature, humidité_air, humidité_sol):
         arrosage = 1
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO info (info_type, info_arduino, info_value, info_date) VALUES('%s','%s','%s','" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "')"
+            sql = "INSERT INTO info (info_type, info_arduino, info_value, info_date) VALUES('%s','%s','%s','" + String_now + "')"
             cursor.execute(sql,(1, arduino_id, temperature))
             cursor.execute(sql,(2, arduino_id, humidité_air))
             cursor.execute(sql,(3, arduino_id, humidité_sol))
             cursor.execute(sql,(4, arduino_id, arrosage))
     finally:     
         connection.close()
+        chaine_CSV = [String_now, str(arduino_id), str(temperature), str(humidité_air), str(humidité_sol),str(arrosage)]
+        print(chaine_CSV)
+        if not CSV_golf.csv_exist(CSV_name) :
+            CSV_golf.inserer(CSV_name, ["Heure","Arduino_id","Temperature","Humidite_air","Humidite_sol","Arrosage"])
+        CSV_golf.inserer(CSV_name, chaine_CSV)
     return (arrosage*arduino_id)
 
 
